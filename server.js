1fe025f83849bd4d42c19ee43dce130d52b89d4a
Javier Olmo <olmogil@gmail.com>
2019-02-11 00:38:46 +0100

    testing

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var reload = require('reload');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 
@@ -24,9 +23,6 @@ app.get('/', function (req, res) {
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 
-
-// Reload code here
-reload(app);
 
 server.listen(app.get('port'), function () {
   console.log('Web server listening on port ' + app.get('port'))
